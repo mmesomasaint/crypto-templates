efe75dda37c1be497c7dd5db2c429194c62c4e11
Mmesoma Saint <mmesomasaint@gmail.com>
2023-02-10 02:52:58 +0100

    Feat: Add darkmode toggle

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,34 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import { useCallback, useState } from 'react'
+import { MdDarkMode } from 'react-icons/md'
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false)
+  const handleDarkMode = useCallback(() => setDarkMode((dark) => !dark), [])
+
   return (
-    <div className=''>
+    <div className={darkMode && 'dark'}>
       <Head>
         <title>Crypto Templates</title>
-        <meta name="description" content="Best templates for crypto websites" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='Best templates for crypto websites' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className=''>
+      <header>
+        <div className='flex justify-end items-center px-5 bg-white dark:bg-gray-900'>
+          <MdDarkMode
+            className={`text-[#023047] dark:text-white text-2xl`}
+            onClick={handleDarkMode}
+          />
+        </div>
+      </header>
+
+      <main className='bg-white dark:bg-gray-600'>
+        Main
       </main>
 
-      <footer className=''>
+      <footer className='bg-white dark:bg-gray-400'>
+        Footer
       </footer>
     </div>
   )
