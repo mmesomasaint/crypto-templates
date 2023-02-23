d71e9f38da0aa93222c4bcbe508e98bbf4e53797
Mmesoma Saint <mmesomasaint@gmail.com>
2023-02-23 03:58:12 +0100

    Feat: Start designing the roadmap section

--- pages/templates/crypto-invest/index.js
+++ pages/templates/crypto-invest/index.js
@@ -594,6 +594,31 @@ export default function Home() {
               </div>
             </div>
           </div>
+          <div className='py-24 bg-black'>
+            <div className='w-[80%] mx-auto text-center'>
+              <div className='w-fit mx-auto'>
+                <h4 className='text-white text-lg font-light leading-normal'>
+                  ROADMAP
+                </h4>
+                <div className='flex gap-0 items-center justify-start'>
+                  <div className='border w-[8px] h-[8px] rounded-full border-white' />
+                  <div className='border-t border-t-white w-8 flex-grow' />
+                  <div className='border w-[8px] h-[8px] rounded-full border-white' />
+                  <div className='border-t border-t-white w-8 flex-grow' />
+                  <div className='border w-[8px] h-[8px] rounded-full border-white' />
+                </div>
+              </div>
+              <div className='mt-4 mb-7'>
+                <h2 className='text-4xl font-semibold text-[#0F69E5] leading-tight'>
+                  Our Strategy & Project Plan
+                </h2>
+              </div>
+              <p className='text-base font-normal leading-tight text-white w-[50%] mx-auto mb-14'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                tempus dui nec euismod congue.
+              </p>
+            </div>
+          </div>
         </div>
       </main>
     </div>
