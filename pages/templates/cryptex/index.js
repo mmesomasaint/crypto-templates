d3e7060558d97362c1b0e7436ed87a3676ebb8dd
Mmesoma Saint <mmesomasaint@gmail.com>
2023-03-20 09:53:14 +0100

    Feat: Fixed the title and icon of cryptex

---
+++ pages/templates/cryptex/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Cryptex</title>
+        <meta
+          name='description'
+          content='create, exchange or collect digital items'
+        />
+        <link rel='icon' href='/imgs/cryptex/favicon.ico' />
+      </Head>
+    </div>
+  )
+}
