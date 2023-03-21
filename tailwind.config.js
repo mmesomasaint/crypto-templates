94e1f9333f0bbeb483f762b809c4cd26f9d41906
Mmesoma Saint <mmesomasaint@gmail.com>
2023-03-21 07:18:10 +0100

    Feat: Name the primary color in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   darkMode: 'class',
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      ...colors,
+      primary: '#F2055C',
+    },
     extend: {},
   },
   plugins: [],
