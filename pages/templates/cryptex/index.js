f01222beb96a26919e9c610e690f8f4afab31556
Mmesoma Saint <mmesomasaint@gmail.com>
2023-03-24 02:13:32 +0100

    Fix: Make nav bar respond to click events

--- pages/templates/cryptex/index.js
+++ pages/templates/cryptex/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState, useCallback } from 'react'
 import {
   BsFillBagFill,
   BsFillPersonFill,
@@ -12,6 +13,9 @@ import { TbBrandTelegram } from 'react-icons/tb'
 import { GiHamburgerMenu } from 'react-icons/gi'
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false)
+  const handleOpen = useCallback(() => setIsOpen((open) => !open), [])
+
   return (
     <div>
       <Head>
@@ -36,7 +40,7 @@ export default function Home() {
               </div>
               <div className='flex justify-between items-center gap-7'>
                 <div
-                  className={`hidden w-full md:w-fit flex-col md:flex-row absolute z-10 top-[99%] left-0 md:static lg:flex justify-evenly items-start md:items-center gap-7 px-[1%] py-5 md:p-0`}
+                  className={`${isOpen ? 'flex bg-black' : 'hidden'} w-full lg:w-fit flex-col lg:flex-row absolute z-10 top-[99%] left-0 lg:static lg:bg-transparent lg:flex justify-evenly items-start lg:items-center gap-7 px-[1%] py-5 lg:p-0`}
                 >
                   <span className='font-normal text-xl leading-none text-primary scale-105 underline underline-offset-[6px]'>
                     <Link href='/templates/cryptex'>Home</Link>
@@ -60,7 +64,7 @@ export default function Home() {
               </div>
               <div className='flex justify-center items-center gap-7'>
                 <div className='block lg:hidden'>
-                  <GiHamburgerMenu className='text-3xl text-white' />
+                  <GiHamburgerMenu className='text-3xl text-white' onClick={handleOpen} />
                 </div>
                 <button className='text-white text-base md:text-lg py-2 md:py-3 px-4 md:px-6 font-medium leading-none bg-transparent border-2 border-primary rounded-full'>
                   Connect Wallet
